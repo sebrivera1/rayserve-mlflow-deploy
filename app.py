23c686769ac6c6b59380496a155dcb6728d70302
Sajan Mehta <SPMehta333@gmail.com>
2025-12-05 20:43:59 -0600

    Added squat first attempt to inputs

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ with gr.Blocks(title="Power Lifting SBD Predictor") as demo:
     health_btn.click(fn=check_health, outputs=health_output)
     submit_btn.click(
         fn=predict,
-        inputs=[name_input, weight_input, squat_input, bench_input, deadlift_input, sex_input],
+        inputs=[name_input, weight_input, squat_input, bench_input, deadlift_input, sex_input, squat_first_attempt],
         outputs=output
     )
 
